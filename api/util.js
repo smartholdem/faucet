a350abdf93bda1a3a6eb29d59a8357f6d80e58bc
technolog <viktor@allz.su>
2018-04-15 03:38:15 +0300

    api util

---
+++ api/util.js
@@ -0,0 +1,27 @@
+"use strict";
+var BigNumber = require("bignumber.js");
+var fs = require("fs");
+
+var alphanumRegex = /^[a-z0-9]+$/i;
+
+exports.reject = (res, status, msg) => {
+    console.log(`Rejected: ${status} - ${msg}`);
+    var resp = {
+        success: false,
+        message: msg
+    }
+    res.status(status);
+    res.send(resp);
+}
+
+exports.isAddress = (addr) => {
+    return alphanumRegex.test(addr) && addr.length == 34 && addr.charAt(0) == 'S';
+};
+
+exports.log = (msg, async) => {
+    console.log(msg);
+    if(async)
+        fs.appendFile(LOG_FILE, msg);
+    else
+        fs.appendFileSync(LOG_FILE, msg);
+};
