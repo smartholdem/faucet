3fa1116d56f919fb1ebed82af02342dc42fb0bf5
technolog <viktor@allz.su>
2018-04-15 12:14:55 +0300

    Isaddr

--- api/util.js
+++ api/util.js
@@ -2,7 +2,7 @@
 var BigNumber = require("bignumber.js");
 var fs = require("fs");
 
-var alphanumRegex = /^[1-9A-Za-z]+$/g;
+var alphanumRegex = /^[S|s]{1}[0-9a-zA-Z]{33}$/g;
 
 exports.reject = (res, status, msg) => {
     console.log(`Rejected: ${status} - ${msg}`);
@@ -19,7 +19,7 @@ exports.isAddress = (addr) => {
     console.log(addr.length);
     console.log(addr.charAt(0));
 
-    return alphanumRegex.test(addr) && addr.length == 34 && addr.charAt(0) == 'S';
+    return alphanumRegex.test(addr); // && addr.length == 34 && addr.charAt(0) == 'S'
 };
 
 exports.log = (msg, async) => {
