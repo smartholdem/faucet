4c65f03bcc119b3a0d97b867dcf2fc1b76d0f03b
technolog <viktor@allz.su>
2018-04-15 12:16:50 +0300

    Isaddr

--- api/util.js
+++ api/util.js
@@ -15,7 +15,6 @@ exports.reject = (res, status, msg) => {
 }
 
 exports.isAddress = (addr) => {
-    console.log(addr);
     console.log(alphanumRegex.test(addr));
     console.log(addr.length);
     console.log(addr.charAt(0));
