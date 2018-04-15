2595736cdf9bd70b9a272e50e4248bcc74046b17
technolog <viktor@allz.su>
2018-04-15 03:39:12 +0300

    api db

---
+++ api/repos/faucetRepo.js
@@ -0,0 +1,139 @@
+"use strict";
+
+exports.getUnpaidBalanceByAddress = (address) => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            con.query("SELECT * FROM faucet.Unpaid_Balances WHERE address = ?", [address], function(err, rows) {
+                con.release();
+                if(!err)
+                    resolve(rows);
+                reject(err);
+            });
+        });
+    });
+};
+
+exports.getAllUnpaidBalances = () => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            con.query("SELECT * FROM faucet.Unpaid_Balances", function(err, rows) {
+                con.release();
+                if(!err)
+                    resolve(rows);
+                reject(err);
+            });
+        });
+    });
+};
+
+exports.sumUnpaidBalance = () => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            con.query("SELECT SUM(pending) FROM faucet.Unpaid_Balances", function(err, rows) {
+                con.release();
+                if(!err)
+                {
+                    var sum = parseFloat(rows["SUM(pending)"]);
+                    if(isNaN(sum))
+                        resolve({sum: 0});
+                    else
+                        resolve({sum: sum});
+                }
+                reject(err);
+            });
+        });
+    });
+};
+
+exports.updateUnpaidBalance = (address, payPerClick) => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            var unpaidBal = {
+                address: address,
+                pending: payPerClick.toString()
+            };
+            con.query("INSERT INTO faucet.Unpaid_Balances set ? ON DUPLICATE KEY UPDATE pending = pending + ?", [unpaidBal, payPerClick], (err, rows) => {
+                con.release();
+                if(!err)
+                    resolve();
+                else
+                {
+                    console.log(err);
+                    reject(err);
+                }
+            });
+        });
+    });
+};
+
+exports.getOverthresholdBalances = (threshold) => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            con.query("SELECT * FROM faucet.Unpaid_Balances WHERE pending >= ?", threshold, function(err, rows) {
+                con.release();
+                if(!err)
+                    resolve(rows);
+                reject(err);
+            });
+        });
+    });
+};
+
+exports.deleteUnpaidBalances = (addrs) => {
+    var joinedAddrs = addrs.map((addr) => `'${addr}'`).join(",");
+    var query = "DELETE FROM faucet.Unpaid_Balances WHERE address in (";
+    query += joinedAddrs + ")";
+
+    getConnection().then((con) => {
+        con.query(query, function(err, rows) {
+            con.release();
+        });
+    });    
+};
+
+exports.getRollTimeByIp = (ip) => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            con.query("SELECT * FROM faucet.Roll_Times WHERE IP = ?", [ip], function(err, rows) {
+                con.release();
+                if(!err)
+                    resolve(rows);
+                reject(err);
+            });
+        });
+    });
+};
+
+exports.getAllRollTimes = () => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            con.query("SELECT * FROM faucet.Roll_Times", function(err, rows) {
+                con.release();
+                if(!err)
+                    resolve(rows);
+                reject(err);
+            });
+        });
+    });
+};
+
+exports.updateRollTime = (IP, rollTime) => {
+    return new Promise((resolve, reject) => {
+        getConnection().then((con) => {
+            var unpaidBal = {
+                IP: IP,
+                lastRoll: rollTime
+            };
+            con.query("INSERT INTO faucet.Roll_Times set ? ON DUPLICATE KEY UPDATE lastRoll = ?", [unpaidBal, rollTime], (err, rows) => {
+                con.release();
+                if(!err)
+                    resolve();
+                else
+                {
+                    console.log(err);
+                    reject(err);
+                }
+            });
+        });
+    });
+};
